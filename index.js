ff16964c1523eb5dcea13b5c6da1f8c7d0037667
Gene Piki <85885450+pikigene01@users.noreply.github.com>
2024-04-06 13:31:47 +0200

    update

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const router = require('./routes/router');
+const express = require("express");
+const router = require("./routes/router");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// mongoose.connect('', ()=>console.log('Connected to db'));
+mongoose.connect(
+  process.env.MongoDBConnect,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Database connected");
+  }
+);
 app.use(express.json());
-app.use('/api/user', router);
+app.use("/api/user", router);
 
-
-app.listen('3000', ()=>console.log('server running'));
+app.listen("3000", () => console.log("server running"));
