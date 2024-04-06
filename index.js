e6ed11bf9daa3b3ea687a51e201e96b398a0b7fc
Gene Piki <85885450+pikigene01@users.noreply.github.com>
2024-04-06 17:53:11 +0200

    update app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fileUpload = require('express-fileupload');
 const router = require("./routes/router");
 const app = express();
 const mongoose = require("mongoose");
@@ -14,6 +15,7 @@ mongoose.connect(
   }
 );
 app.use(express.json());
+app.use(fileUpload());
 app.use("/api/user", router);
 
 app.listen("3000", () => console.log("server running"));
--- routes/router.js
+++ routes/router.js
@@ -33,4 +33,23 @@ router.post("/register", async (req, res) => {
   res.send("Register");
 });
 
+app.post("/upload", (req, res) => {
+  // Check if files were uploaded
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  // Access the uploaded file
+  const file = req.files.myFile;
+
+  // Move the file to the desired location
+  file.mv("/path/to/destination/filename.ext", (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send("File uploaded successfully");
+  });
+});
+
 module.exports = router;
